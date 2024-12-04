77b8ab25ad7a06e3a49ef8cddbde1c36b624a739
sjin824 <sjin824@aucklanduni.ac.nz>
2024-12-04 11:09:31 +0000

    Prepare for modifications.

---
+++ PreSumm/presumm/__init__.py
@@ -0,0 +1,20 @@
+from . import distributed
+from .models import model_builder
+
+from .models.model_builder import AbsSummarizer, ExtSummarizer
+
+from .models.loss import abs_loss
+from .models.predictor import build_predictor
+from .models.trainer import build_trainer as build_trainer_abs
+from .models.trainer_ext import build_trainer as build_trainer_ext
+
+__all__ = [
+    "distributed",
+    "model_builder",
+    "AbsSummarizer",
+    "ExtSummarizer",
+    "abs_loss",
+    "build_predictor",
+    "build_trainer_abs",
+    "build_trainer_ext"
+]
---
+++ PreSumm/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="presumm",
+    version="1.0.0",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "multiprocess", # ==0.70.9
+        "numpy", # ==1.17.2
+        "pyrouge", # ==0.1.3
+        "pytorch-transformers", # ==1.2.0
+        "tensorboardX", # ==1.9
+        "torch" # ==1.1.0
+    ],
+)
